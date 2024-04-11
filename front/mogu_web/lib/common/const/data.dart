37b770e6eb551d9abe826851bb61eaa5bb678fb0
Dcool <90443691+LSW2717@users.noreply.github.com>
2024-04-11 15:17:57 +0900

    fix: fix bug

--- front/mogu_web/lib/common/const/data.dart
+++ front/mogu_web/lib/common/const/data.dart
@@ -2,4 +2,6 @@ const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN';
 const REFRESH_TOKEN_KEY = 'REFRESH_TOKEN';
 
 // localhost
-final ip = '211.45.167.245:80';
+// final ip = '211.45.167.245:80';
+
+final ip = 'localhost:8080';
